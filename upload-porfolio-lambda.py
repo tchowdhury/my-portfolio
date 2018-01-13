efbc369fd7445b33c3310e85c02a87bb9df63944
Tapajoyti Chowdhury <tchowdhury@tnsiit.com>
2018-01-13 11:38:14 +1100

    Update portfolio function for Lambda with exception handler

--- upload-porfolio-lambda.py
+++ upload-porfolio-lambda.py
@@ -6,24 +6,32 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:294911564117:codeDeployTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.gogo-gadget.com.au')
-    build_bucket = s3.Bucket('portfoliobuild.gogo-gadget.com.au')
+    try:
+        s3 = boto3.resource('s3')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('Portfoliobuild.zip',portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.gogo-gadget.com.au')
+        build_bucket = s3.Bucket('portfoliobuild.gogo-gadget.com.au')
 
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('Portfoliobuild.zip',portfolio_zip)
 
 
-    print 'Job dobe!'
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+
+        print 'Job dobe!'
+        topic.publish(Subject="Code Deployed",Message="Code Deployed Successfully")
+    except:
+        print 'Job failed!'
+        topic.publish(Subject="Code Deploy Failed",Message="Code Deploy Failed")
 
     # TODO implement
     return 'Hello from Lambda'
